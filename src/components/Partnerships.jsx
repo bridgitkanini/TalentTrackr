150fafc2f309d843ebf6442db67630fb08a9359c
bkm-code <bridgit.k.mutinda@gmail.com>
2024-06-12 13:59:47 +0300

    Create Partnerships section.

---
+++ src/components/Partnerships.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Partnerships = () => {
+  return (
+    <main id="partnerships">
+      <div className="flex flex-col items-center justify-center h-[70vh] gap-10 mx-auto md:w-[750px]">
+        <h1 className="text-center text-xl sm:text-3xl md:text-5xl">
+          Brand Identity
+        </h1>
+        <p>
+          Roots & Radiance Photography is a beacon of creativity, capturing the
+          essence of beauty with a vibrant and unique lens. We specialize in
+          stunning visuals that celebrate the natural elegance and diversity of
+          our subjects. Our collaborations with top advertising creatives result
+          in impactful, memorable campaigns. With a focus on authenticity and
+          artistry, we highlight the brilliance in every moment. Our
+          state-of-the-art studio and expert photographers ensure every shoot is
+          a masterpiece. Discover the radiance within and let us tell your story
+          through powerful imagery.
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Partnerships;
